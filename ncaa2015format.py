fc0981e4229ee222a1d90044b0a8ff2b67f35597
Brian Carlo <brian.carlo@gmail.com>
2015-04-05 16:21:39 -0700

    boom. got it.

--- ncaa2015format.py
+++ ncaa2015format.py
@@ -1,24 +1,39 @@
 import csv
 
-f = open('ncaa2015.csv', 'rU')
+# f = open('ncaa2015.csv', 'rU')
+# reader = csv.reader(f)
+
+# scores = []
+
+# for line in reader:
+# 	newline = []
+# 	line = line[0].split(',')
+# 	for boxline in line:
+# 		score = boxline[-2:]
+# 		team = boxline[:-2]
+# 		newline.append(team.strip())
+# 		newline.append(score)
+# 	scores.append(newline)
+
+# for score in scores:
+# 	print score
+
+# f = open("ncaa2015new.csv", "w")
+# writer = csv.writer(f)
+# for score in scores:
+# 	writer.writerow(score)
+
+f = open('ncaa2015new.csv', 'rU')
 reader = csv.reader(f)
 
-scores = []
+
+f = open('ncaa2015final.txt', 'w')
 
 for line in reader:
-	newline = []
-	line = line[0].split(',')
-	for boxline in line:
-		score = boxline[-2:]
-		team = boxline[:-2]
-		newline.append(team.strip())
-		newline.append(score)
-	scores.append(newline)
+	line[3] = str(line[3])
+	line[4] = str(line[4])
+	line[5] = str(line[5])
+	print "[" + ",".join(line[0:3]) + ',"' + line[3] + '","' + line[4] + '","' + line[5] + '"],'
+	# print ",".join(line[3:6])
 
-for score in scores:
-	print score
 
-f = open("ncaa2015new.csv", "w")
-writer = csv.writer(f)
-for score in scores:
-	writer.writerow(score)
