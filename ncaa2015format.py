617ea8fc51fcc1762ef8c0c7afad541d631213c0
Brian Carlo <brian.carlo@gmail.com>
2015-04-05 21:43:45 -0700

    added 2011 data

--- ncaa2015format.py
+++ ncaa2015format.py
@@ -1,6 +1,6 @@
 import csv
 
-# f = open('ncaa2012.csv', 'rU')
+# f = open('ncaa2011.csv', 'rU')
 # reader = csv.reader(f)
 
 # scores = []
@@ -18,22 +18,18 @@ import csv
 # for score in scores:
 # 	print score
 
-# f = open("ncaa2012new.csv", "w")
+# f = open("ncaa2011new.csv", "w")
 # writer = csv.writer(f)
 # for score in scores:
 # 	writer.writerow(score)
 
-f = open('ncaa2012new.csv', 'rU')
+f = open('ncaa2011new.csv', 'rU')
 reader = csv.reader(f)
-
-
-# f = open('ncaa2012final.txt', 'w')
 
 for line in reader:
 	line[3] = str(line[3])
 	line[4] = str(line[4])
 	line[5] = str(line[5])
 	print "[" + ",".join(line[0:3]) + ',"' + line[3] + '","' + line[4] + '","' + line[5] + '"],'
-	# print ",".join(line[3:6])
 
 
